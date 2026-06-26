75f76e5a4cd19c11f0d6c2e99476498217e3a73d
Yeager140 <ackerman140levi@gmail.com>
2026-06-27 00:35:39 +0530

    week - 02

---
+++ Week_02_Spring_Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void executeDataQuery() {
+        System.out.println("[REPOS ENGINE]: Successfully queried persistent database catalog tables.");
+    }
+}
